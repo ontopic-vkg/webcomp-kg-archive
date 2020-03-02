cfad140f1aeaf7bb50db12f54da8d69dff701da9
Guohui Xiao <xiao@inf.unibz.it>
2020-03-02 11:23:07 +0100

    kg-table polished

--- src/app/kg-table/kg-table.component.ts
+++ src/app/kg-table/kg-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, ElementRef, Input, OnInit} from '@angular/core';
 import {SparqlService} from "../sparql.service";
 import {Observable} from "rxjs";
 import {SelectResults} from "../../model/SelectResults";
@@ -9,56 +9,38 @@ import {SelectResults} from "../../model/SelectResults";
   templateUrl: './kg-table.component.html',
   styleUrls: ['./kg-table.component.css']
 })
-export class KgTableComponent {
+export class KgTableComponent implements OnInit {
+
+  @Input() endpoint: string;
+
   @Input() signupTitle = 'Sign up for our newsletter';
-  @Input() thankyouMessage = 'Thanks!';
+  @Input() errorMessage = '';
+
+  @Input() query: string;
+
   results: SelectResults;
 
-  formData = { name: '', email: '', query: 'PREFIX schema: <http://schema.org/>\n' +
-      'PREFIX geo: <http://www.opengis.net/ont/geosparql#>\n' +
-      '\n' +
-      'SELECT ?h ?pos ?posLabel ?posColor\n' +
-      'WHERE {\n' +
-      '  ?h a schema:LodgingBusiness ;\n' +
-      '     geo:asWKT ?pos ;\n' +
-      '     schema:name ?posLabel ;\n' +
-      '     schema:address ?a .\n' +
-      '  # ?a schema:postalCode "39100" . # Uncomment for Bolzano only\n' +
-      '  FILTER (lang(?posLabel) = \'de\')\n' +
-      '\n' +
-      '  # Colors\n' +
-      '  OPTIONAL {\n' +
-      '    ?h a schema:Campground .\n' +
-      '    BIND("chlorophyll,0.5" AS ?posColor) # Green\n' +
-      '  }\n' +
-      '    OPTIONAL {\n' +
-      '    ?h a schema:BedAndBreakfast .\n' +
-      '    BIND("viridis,0.1" AS ?posColor) # Purple\n' +
-      '  }\n' +
-      '  OPTIONAL {\n' +
-      '    ?h a schema:Hotel .\n' +
-      '    BIND("jet,0.3" AS ?posColor) # Light blue\n' +
-      '  }\n' +
-      '  OPTIONAL {\n' +
-      '    ?h a schema:Hostel .\n' +
-      '    BIND("jet,0.8" AS ?posColor) # Red\n' +
-      '  }\n' +
-      '}\n' +
-      'LIMIT 500\n' };
-
   formSubmitted = false;
+  finished: boolean = false;
 
-  constructor(private sparqlService: SparqlService) { }
+  constructor(private sparqlService: SparqlService, private el: ElementRef) {
 
-  onSubmit() {
-    const results: Observable<SelectResults> = this.sparqlService.select(this.formData.query);
+  }
+
+  ngOnInit(): void {
+
+    this.sparqlService.endpoint = this.endpoint;
+
+    // let myCurrentContent:string = this.el.nativeElement.innerHTML;
+    // console.log(myCurrentContent)
+
+    const results: Observable<SelectResults> = this.sparqlService.select(this.query);
     results.subscribe(value => {
+      this.finished = true;
       this.results = value;
-      this.thankyouMessage = JSON.stringify(value);
-      this.formSubmitted = true;
     }, error => {
-      this.thankyouMessage = JSON.stringify(error);
-      this.formSubmitted = true;
+      this.finished = true;
+      this.errorMessage = JSON.stringify(error);
     });
 
   }
--- src/app/sparql.service.ts
+++ src/app/sparql.service.ts
@@ -6,6 +6,15 @@ import {SelectResults} from "../model/SelectResults";
   providedIn: 'root'
 })
 export class SparqlService {
+  get endpoint(): string {
+    return this._endpoint;
+  }
+
+  set endpoint(value: string) {
+    this._endpoint = value;
+  }
+
+  private _endpoint; // = "https://sparql.opendatahub.bz.it/sparql";
 
   constructor(private http: HttpClient) {
   }
@@ -13,7 +22,8 @@ export class SparqlService {
   select(sparql: string) {
     const requestBody = "query=" + encodeURIComponent(sparql) +
       "&Accept=" + encodeURIComponent('application/sparql-results+json');
-    return this.http.post<SelectResults>("https://sparql.opendatahub.bz.it/sparql", requestBody,{
+    console.log(requestBody);
+    return this.http.post<SelectResults>(this._endpoint, requestBody, {
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
       }),
