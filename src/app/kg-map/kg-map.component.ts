23c0c959080abeb0385faea695f73fec6ba9ce08
Guohui Xiao <xiao@inf.unibz.it>
2020-03-04 12:34:56 +0100

    reimplement popup following official guide

--- src/app/kg-map/kg-map.component.ts
+++ src/app/kg-map/kg-map.component.ts
@@ -2,8 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {SelectResultSet} from "../../model/sparql";
 import {SparqlService} from "../sparql.service";
 import {Observable} from "rxjs";
-import * as $ from "jquery";
-import {useGeographic} from 'ol/proj';
+import {toLonLat, useGeographic} from 'ol/proj';
 import {Map, Overlay, View} from 'ol';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import OSM from 'ol/source/OSM';
@@ -13,9 +12,8 @@ import {MousePosition, OverviewMap, Zoom, FullScreen} from "ol/control";
 import {Circle, Fill, Style} from "ol/style";
 import {defaults as defaultInteractions, DragPan} from 'ol/interaction';
 
-import 'bootstrap';
 import {Point} from "ol/geom";
-import OverlayPositioning from "ol/OverlayPositioning";
+
 
 
 @Component({
@@ -63,7 +61,7 @@ export class KgMapComponent implements OnInit {
     // bolzano
     const place = [11.33982, 46.49067];
 
-    const overlay = new VectorLayer({
+    const vectorLayer = new VectorLayer({
       source: new VectorSource({
         features: []
       }),
@@ -74,6 +72,36 @@ export class KgMapComponent implements OnInit {
         })
       })
     });
+
+    const osmLayer = new TileLayer({
+      source: new OSM()
+    });
+
+
+    /**
+     * Elements that make up the popup.
+     */
+    var container = document.getElementById('popup');
+    var content = document.getElementById('popup-content');
+    var closer = document.getElementById('popup-closer');
+
+
+    /**
+     * Create an overlay to anchor the popup to the map.
+     */
+    var overlay = new Overlay({
+      element: container,
+      autoPan: true,
+      autoPanAnimation: {
+        duration: 250
+      }
+    });
+
+    closer.onclick = function () {
+      overlay.setPosition(undefined);
+      closer.blur();
+      return false;
+    };
 
     var map = new Map({
       interactions: defaultInteractions({
@@ -86,59 +114,30 @@ export class KgMapComponent implements OnInit {
         zoom: 8
       }),
       layers: [
-        new TileLayer({
-          source: new OSM()
-        }),
+        osmLayer,
+        vectorLayer
+      ],
+      overlays: [
         overlay
       ],
-      // interactions: [
-      //   new DragPan(),
-      // ],
-      // controls: [
-      //   new OverviewMap(),
-      //   new Zoom(),
-      //   new MousePosition(),
-      //   new FullScreen()
-      // ]
+      controls: [
+        new OverviewMap(),
+        new Zoom(),
+        new MousePosition(),
+        new FullScreen()
+      ]
     });
 
-    var element = document.getElementById('popup');
+    map.on('singleclick', function(evt) {
+      var coordinate = evt.coordinate;
 
-    var popup = new Overlay({
-      element: element,
-      positioning: OverlayPositioning.CENTER_CENTER, //'bottom-center',
-      stopEvent: false,
-      offset: [0, -10]
-    });
-    map.addOverlay(popup);
-
-    function formatCoordinate(coordinate) {
-      return ("\n    <table>\n      <tbody>\n        <tr><th>lon</th><td>" + (coordinate[0].toFixed(2)) + "</td></tr>\n        <tr><th>lat</th><td>" + (coordinate[1].toFixed(2)) + "</td></tr>\n      </tbody>\n    </table>");
-    }
-
-    var info = document.getElementById('info');
-    map.on('moveend', function() {
-      var view = map.getView();
-      var center = view.getCenter();
-      info.innerHTML = formatCoordinate(center);
-    });
-
-    map.on('click', function(event) {
-      var feature = map.getFeaturesAtPixel(event.pixel)[0];
-      if (feature) {
-        var coordinate = (feature.getGeometry() as Point).getCoordinates();
-        popup.setPosition(coordinate);
-        console.log(formatCoordinate(coordinate));
-        ($(element) as any).popover({
-          placement: 'top',
-          title: feature.get('label'),
-          html: true,
-          content: formatCoordinate(coordinate)
-        });
-        ($(element) as any).popover('show');
-      } else {
-        ($(element) as any).popover('dispose');
-      }
+      var feature = map.getFeaturesAtPixel(evt.pixel)[0];
+        if (feature) {
+          const coordinate = (feature.getGeometry() as Point).getCoordinates();
+          const label = feature.get('label');
+          content.innerHTML = label;
+          overlay.setPosition(coordinate);
+        }
     });
 
     map.on('pointermove', function(event) {
@@ -149,7 +148,6 @@ export class KgMapComponent implements OnInit {
       }
     });
 
-
-    return overlay;
+    return vectorLayer;
   }
 }
